cd153f6e8220e4e1e3b30f97b3874672e2f3ea42
basit2023 <127020924+basit2023@users.noreply.github.com>
2023-11-17 22:34:26 +0500

    Update Homepage.js

--- src/components/Pages/Home/Homepage.js
+++ src/components/Pages/Home/Homepage.js
@@ -160,7 +160,7 @@ const HomePage = () => {
             {products?.map((p) => (
               <div className="card m-2" key={p._id}>
                 <img
-                  src={`/product/product-photo/${p._id}`}
+                  src={`https://kind-pink-nightingale-sock.cyclic.app/product/product-photo/${p._id}`}
                   className="card-img-top"
                   alt={p.name}
                 />
