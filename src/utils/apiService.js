9a219bf32372af37c48d98a96d1f53299b7f9e38
basit2023 <127020924+basit2023@users.noreply.github.com>
2023-11-17 19:14:21 +0500

    Update apiService.js

--- src/utils/apiService.js
+++ src/utils/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiService = axios.create({
-    baseURL: "http://localhost:9000/api/",
+    baseURL: "https://kind-pink-nightingale-sock.cyclic.app/api",
     withCredentials: true
 })
 
